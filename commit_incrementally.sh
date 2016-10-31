caab2ff5ebcfd311091a8c8a7853997664683170
J. R. Schmid <jrs+git@weitnahbei.de>
2016-10-31 17:59:29 +0100

    Another manual try.

--- commit_incrementally.sh
+++ commit_incrementally.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-for i in {extracted,processed}/*; do git add $i && git commit -uno -a -m "$1" && git push; done
+# for i in {extracted,processed}/*; do git add $i && git commit -uno -a -m "$1" && git push; done
+git add . && git commit -a -m "Autocommit" && git push
